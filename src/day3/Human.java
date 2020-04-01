0a9367bc36423bae2d5aa027c10b33b95a8671a9
Roman Iliuchik <r.iliuchik@gmail.com>
2020-04-01 13:22:15 +0300

    Add class Human from day3

---
+++ src/day3/Human.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Human {
+    private int age;
+    private boolean abilityToSpeak;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAbilityToSpeak() {
+        return abilityToSpeak;
+    }
+
+    public void setAbilityToSpeak(boolean abilityToSpeak) {
+        this.abilityToSpeak = abilityToSpeak;
+    }
+}
