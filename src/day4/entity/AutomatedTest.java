ce2d20bb35d114f262f8c875e27346c38f3ab037
Roman Iliuchik <r.iliuchik@gmail.com>
2020-04-16 16:39:48 +0300

    class AutomatedTest for day4 is fixed

--- src/day4/entity/AutomatedTest.java
+++ src/day4/entity/AutomatedTest.java
@@ -2,7 +2,6 @@ package day4.entity;
 
 public class AutomatedTest extends Test {
     public AutomatedTest(TestLevel testLevel, int instability) {
-        super(testLevel);
-        this.setInstability(instability);
+        super(testLevel, instability);
     }
 }
