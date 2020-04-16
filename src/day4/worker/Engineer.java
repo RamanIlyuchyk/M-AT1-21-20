6fb415b208e4465e1e5437309b041bdbf53f62df
Roman Iliuchik <r.iliuchik@gmail.com>
2020-04-16 17:04:11 +0300

    class Engineer is fixed

--- src/day4/worker/Engineer.java
+++ src/day4/worker/Engineer.java
@@ -3,24 +3,33 @@ package day4.worker;
 import day4.entity.Result;
 import day4.entity.Test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Engineer {
-    protected int maxSkill = 10;
-    protected int anxiety = 3;
+    private int anxiety = 3;
     private int skill;
 
     public int getAnxiety() {
         return anxiety;
     }
 
+    public void setAnxiety(int anxiety) {
+        this.anxiety = anxiety;
+    }
+
     public int getSkill() {
         return skill;
     }
 
-    public void setSkill() {
-        this.skill = (int) (1 + Math.random() * maxSkill);
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
+
+    public void setRandomSkill() {
+        this.skill = ThreadLocalRandom.current().nextInt(1, 11);
     }
 
     public Result executeTest(Test test) {
-        return null;
+        return test.apply(this);
     }
 }
